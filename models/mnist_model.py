f0580e376864e07f6b3b4919dce6852101f07c37
Tabish Sayyed <157610987+Tabish0007@users.noreply.github.com>
2025-09-21 01:54:14 +0200

    Create mnist_model.py

---
+++ models/mnist_model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+
+# Simple CNN for MNIST
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, 1)
+        self.conv2 = nn.Conv2d(16, 32, 3, 1)
+        self.fc1 = nn.Linear(32*5*5, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 32*5*5)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Load MNIST dataset
+def load_mnist(batch_size=64):
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = MNIST(root='./data', train=True, download=True, transform=transform)
+    test_dataset = MNIST(root='./data', train=False, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return train_loader, test_loader
